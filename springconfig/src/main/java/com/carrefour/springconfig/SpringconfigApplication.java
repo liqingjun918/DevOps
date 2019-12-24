b59c27764fd8a526967e4bb097b855c6cc92dd87
liqingjun918 <liqingjun918@163.com>
2019-12-24 16:42:45 +0800

    submit at 4:42pm

--- springconfig/src/main/java/com/carrefour/springconfig/SpringconfigApplication.java
+++ springconfig/src/main/java/com/carrefour/springconfig/SpringconfigApplication.java
@@ -1,9 +1,11 @@
 package com.carrefour.springconfig;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringCloudApplication
+@EnableConfigServer
 public class SpringconfigApplication {
 
     public static void main(String[] args) {
